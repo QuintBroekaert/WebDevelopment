46fa3455d5c0b85e3f842ba0a3bd002c40a0111e
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-16 01:08:03 +0100

    final substring

--- semester2/Labo2/substring/scripts/index.js
+++ semester2/Labo2/substring/scripts/index.js
@@ -1,15 +1,15 @@
 const setup = () => {
-    const createSubString = (startpoint, length, input) => {
-        return input.substring(startpoint, startpoint + length);
+    const createSubString = (startpoint, endpoint, input) => {
+        return input.substring(startpoint, endpoint);
     };
 
     let btnSubstring = document.getElementById("press");
     btnSubstring.addEventListener("click", () => {
         let startpoint = document.getElementById("startPosition").value;
-        let length = document.getElementById("length").value;
+        let endpoint = document.getElementById("endposition").value;
         let input = document.getElementById("stringInput").value;
 		let output = document.getElementById("output");
-		output.innerHTML = createSubString(startpoint, length, input);
+		output.innerHTML = createSubString(startpoint, endpoint, input);
     });
 };
 
