4b8c9a00611d3c54257e3ef5c55a319a6c80c284
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-19 17:31:15 +0100

    update Labo 3

---
+++ semester2/Labo3/Producten/scripts/index.js
@@ -0,0 +1,12 @@
+const setup = () => {
+	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+}
+let prijzen = document.getElementsByClassName("prijs");
+let aantalInput = document.getElementsByClassName("aantalInput");
+let subtotalent = document.getElementsByClassName("subtotaal");
+window.addEventListener("load", setup);
+const calc = () => {
+    for (let i = 0; i < aantalInput.length; i++){
+
+    }
+}
