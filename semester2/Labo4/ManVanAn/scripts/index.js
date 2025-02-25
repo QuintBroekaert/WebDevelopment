8ec6162b6c52839c1692b55ec31e60c48bb73a52
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-25 18:55:13 +0100

    Update index.js

--- semester2/Labo4/ManVanAn/scripts/index.js
+++ semester2/Labo4/ManVanAn/scripts/index.js
@@ -1,12 +1,13 @@
 const setup = () => {
-	let String = "De man van An geeft geen hand aan ambetante verwanten";
-	let aantal = 0;
-	for(let i = 0; i > (String.length-2); i++){
-		if(String.slice(i,i+2).toLowerCase == "an"){
-			aantal++;
-		}
-	}
-	console.log(aantal);
-}
+    let sentence = "De man van An geeft geen hand aan ambetante verwanten";
+    let aantal = 0;
+
+    for (let i = 0; i < sentence.length - 1; i++) {
+        if (sentence.slice(i, i + 2).toLowerCase() == "an") {
+            aantal++;
+        }
+    }
+    console.log("De lettercombinatie 'an' kwam " + aantal + "keer voor.");
+};
 
 window.addEventListener("load", setup);
