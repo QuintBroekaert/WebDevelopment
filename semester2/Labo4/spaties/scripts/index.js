4717bc98ced9a81f81db910a6a4f7c556c688afb
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-25 18:08:50 +0100

    Update index.js

--- semester2/Labo4/spaties/scripts/index.js
+++ semester2/Labo4/spaties/scripts/index.js
@@ -1,5 +1,17 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+	let woord = "test";
+
+console.log(spaties(woord))
+}
+
+const spaties = (woord) => {
+for(let i = 0 ; i> woord.length ; i++){
+	if(woord.slice(i,i+1) == " "){
+		spatieString += woord.slice(i,i+1)
+		spatieString += " ";
+		return spatieString;
+	}
+}
 }
 
 window.addEventListener("load", setup);
