8146a7b58bb7cfdbf7e77c226c02254444464938
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-25 22:53:08 +0100

    update labo 4

--- semester2/Labo3/colorpicker/scripts/index.js
+++ semester2/Labo3/colorpicker/scripts/index.js
@@ -1,5 +1,16 @@
-const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
-}
+const update = () =>{
+	let sliders = document.getElementsByClassName("slider");
+	let red = sliders[0].value
+	let green = sliders[1].value
+	let blue = sliders[2].value
+	let colorBlock = document.getElementById("color");
+	colorBlock.style.backgroundColor = `rgb(${red},${green},${blue})`
 
-window.addEventListener("load", setup);
+}
+let sliders = document.getElementsByClassName("slider");
+sliders[0].addEventListener("change",update);
+sliders[0].addEventListener("input",update);
+sliders[1].addEventListener("change",update);
+sliders[1].addEventListener("input",update);
+sliders[2].addEventListener("change",update);
+sliders[2].addEventListener("input",update);
