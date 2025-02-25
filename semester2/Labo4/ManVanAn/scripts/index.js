b6368ae3f64b10696de9b1a6872d325c8a13966d
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-25 18:38:08 +0100

    Man van an opdracht update

--- semester2/Labo4/ManVanAn/scripts/index.js
+++ semester2/Labo4/ManVanAn/scripts/index.js
@@ -1,5 +1,12 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+	let String = "De man van An geeft geen hand aan ambetante verwanten";
+	let aantal = 0;
+	for(let i = 0; i > (String.length-2); i++){
+		if(String.slice(i,i+2).toLowerCase == "an"){
+			aantal++;
+		}
+	}
+	console.log(aantal);
 }
 
 window.addEventListener("load", setup);
