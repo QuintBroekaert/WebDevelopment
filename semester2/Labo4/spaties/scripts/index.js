e71106998f9dbc799655a7e3566a14e59a13fd64
QuintBroekaert <quint.broekaert@student.vives.be>
2025-02-27 17:50:00 +0100

    opdracht spaties

--- semester2/Labo4/spaties/scripts/index.js
+++ semester2/Labo4/spaties/scripts/index.js
@@ -1,17 +1,17 @@
-const setup = () => {
-	let woord = "test";
-
-console.log(spaties(woord))
-}
-
-const spaties = (woord) => {
-for(let i = 0 ; i> woord.length ; i++){
-	if(woord.slice(i,i+1) == " "){
+const spaties = () => {
+	let textbox = document.getElementById("woord")
+	let woord = textbox.value
+	let spatieString = "";
+for(let i = 0 ; i < woord.length ; i++){
+	if(woord.slice(i,i+1) !== " "){
 		spatieString += woord.slice(i,i+1)
 		spatieString += " ";
-		return spatieString;
 	}
 }
+console.log(spatieString)
 }
-
-window.addEventListener("load", setup);
+const setup = () =>{
+	let btn = document.getElementById("btn");
+btn.addEventListener("click",spaties);
+}
+window.addEventListener("load",setup)
