598cb23acb80b6ee943fc9d3dc0b8c43e9776e9b
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-12 12:12:54 +0100

    opdracht de en het

--- semester2/Labo5/DeEnHet/scripts/index.js
+++ semester2/Labo5/DeEnHet/scripts/index.js
@@ -1,5 +1,21 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+    let tekst = "Gisteren zat de jongen op de stoep en at de helft van de appel";
+    swap(tekst)
+
+}
+const swap = (tekst) => {
+    console.log("test")
+    let uitkomst =""
+    for(let i = 0 ; i < tekst.length-2;  i++){
+        if(tekst.slice(i,(i+2)).toLowerCase() == "de"){
+            uitkomst +="het"
+            i++
+        }
+        else{
+            uitkomst += tekst.slice(i,(i+1));
+        }
+    }
+    console.log(uitkomst)
 }
 
 window.addEventListener("load", setup);
