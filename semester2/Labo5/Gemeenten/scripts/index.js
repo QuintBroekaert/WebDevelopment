ac7711334fc06f791586f75dedf26506ee51dec9
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-13 14:57:29 +0100

    gemeenten opdracht

--- semester2/Labo5/Gemeenten/scripts/index.js
+++ semester2/Labo5/Gemeenten/scripts/index.js
@@ -1,13 +1,27 @@
 const setup = () => {
-	let Gemeenten = {}
-	let stop = false
-	while(!stop){
-		let input = prompt("Geef een gemeente in:")
-	if(input == "stop"){
-		console.log("hammer time!")
-		stop = true
-	}
-	}
-}
+    let Gemeenten = [];
+    let stop = false;
+    let select = document.getElementById("gemeenten");
+
+    while (!stop) {
+        let input = prompt("Geef een gemeente in: ");
+
+        if (input === "stop") {
+            stop = true;
+        } else if (input) {  // Lege ingaven vermijden
+            Gemeenten.push(input);
+        }
+    }
+
+    // sortering
+    Gemeenten.sort();
+
+    // voeg de gesorteerde steden toe:
+    Gemeenten.forEach(gemeente => {
+        let option = document.createElement("option");
+        option.textContent = gemeente;
+        select.appendChild(option);
+    });
+};
 
 window.addEventListener("load", setup);
