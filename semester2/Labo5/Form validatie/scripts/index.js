c8e473afef58be75674464bd194a3e1a7aa1338c
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-15 12:33:26 +0100

    updqte for;ms deel 2

--- semester2/Labo5/Form validatie/scripts/index.js
+++ semester2/Labo5/Form validatie/scripts/index.js
@@ -1,5 +1,26 @@
-const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+const setup = () =>{
+let form = document.getElementById("form")
+form.addEventListener("submit",getdata)
 }
+const getdata = () => {
+	//voornaam ophalen en valideren.
+	let voornaam = document.getElementById("voornaam").value;
+if(voornaam.length > 30){
+	let voornaamWarning = document.getElementById("voornaamWarning");
+	voornaamWarning.textContent = "Naam is te groot";
+	voornaamWarning.classList.toggle("onzichtbaar");
+}
+//Famillienaam ophalen en valideren
+	let famillienaam = document.getElementById("famillienaam").value;
+	if(famillienaam.length > 50){
+		let WarnnigFamillienaam = document.getElementById("WarnnigFamillienaam");
+		WarnnigFamillienaam.textContent = "de famillienaam is te lang.";
+		WarnnigFamillienaam.classList.toggle("onzichtbaar")
+	}
+	let Geboortedatum = document.getElementById("Geboortedatum").value;
+	if(Geboortedatum ==)
 
-window.addEventListener("load", setup);
+}
+window.addEventListener("load",setup)
+
+
