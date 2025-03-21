683a05883e98448d257d0373061d4e2249a0ef9a
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-21 13:06:01 +0100

    update extras

--- semester2/Labo6/Extra oefeningen/Oefening2/Script/Script.js
+++ semester2/Labo6/Extra oefeningen/Oefening2/Script/Script.js
@@ -1,7 +1,9 @@
 const setup = () =>{
     let list = document.querySelectorAll("li");
     for(let i = 0 ; i < list.length ; i++){
-        
+
         list[i].style.backgroundColor = "red";
     }
+    let img = document.createElement("img");
+    img.src = "images/"
 }
