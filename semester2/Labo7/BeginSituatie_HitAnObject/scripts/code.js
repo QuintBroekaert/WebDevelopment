f1d9152073bd8102a9f95822158e7b7bf5d124d6
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-26 16:48:05 +0100

    update labo7

--- semester2/Labo7/BeginSituatie_HitAnObject/scripts/code.js
+++ semester2/Labo7/BeginSituatie_HitAnObject/scripts/code.js
@@ -1,13 +1,37 @@
+const startGame = () => {
+    var global = {
+        IMAGE_COUNT: 5, // aantal figuren
+        IMAGE_SIZE: 48, // grootte van de figuur
+        IMAGE_PATH_PREFIX: "images/", // map van de figuren
+        IMAGE_PATH_SUFFIX: ".png", // extensie van de figuren
+        MOVE_DELAY: 3000, // aantal milliseconden voor een nieuwe afbeelding verschijnt
+        score: 0, // aantal hits
+        timeoutId: 0 // id van de timeout timer, zodat we die kunnen annuleren
+    }
+    let timer = setInterval(setTarget,1000);
+}
+const setTarget = () => {
+    let playField = document.getElementById("playField");
+    let bomb = document.createElement("img");
+    bomb.style.width = (global.IMAGE_SIZE + "px");
+    bomb.style.height = (global.IMAGE_SIZE + "px");
+    bomb.src = "images/0.png";
+    bomb.alt = "target";
+    bomb.addEventListener("click", click);
+    playField.appendChild(bomb);
 
+}
+const click = () =>{
+    window.alert("You clicked");
+}
+const endGame = () => {
+    window.alert("you are dead");
+    let x = Math.round(Math.random()*600);
+    let y = Math.round(Math.random()*800);
+    bomb.style.marginLeft = x;
+    bomb.style.marginBottom = y;
+}
 
-
-const setup = () => {
-
-};
-
-
-
-
-window.addEventListener("load", setup);
+window.addEventListener("load", startGame);
 
 
