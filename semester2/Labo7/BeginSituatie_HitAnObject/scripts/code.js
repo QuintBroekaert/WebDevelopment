23000b020f2c97b3b73471f79ec60a4cb18da6be
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-27 23:23:50 +0100

    Update code.js

--- semester2/Labo7/BeginSituatie_HitAnObject/scripts/code.js
+++ semester2/Labo7/BeginSituatie_HitAnObject/scripts/code.js
@@ -10,30 +10,34 @@ const startGame = () => {
     };
 }
     window.addEventListener("load",startGame);
-    const createObject = () =>{
+    const createObjects = () =>{
         let field = document.getElementById("playField");
         let object = document.createElement("img");
         object.src = global.IMAGE_PATH_PREFIX + index + global.IMAGE_PATH_SUFFIX;
         object.addEventListener("click", checkState)
         field.append(object);
+        global.timeoutId = setTimeout(wissel, global.MOVE_DELAY)
     }
     const wissel = () =>{
         let field = document.getElementById("playField");
         let objects = document.childNodes;
+        if(objects.length > 0){
         for(let i = 0; i < objects.length; i++){
             let x = Math.random*800;
             x = Math.ceil(x);
             let y = Math.random*600;
             y = Math.ceil(y);
             let temp = objects[i];
-            temp.style.marginLeft(x + "px");
-            temp.style.marginBottom(y + "px");
+            temp.style.marginLeft = (x + "px"); 
+            temp.style.marginBottom = (y + "px");
+        }
         }
     }
     const checkState = () =>{
         if(this.src == "images/0.png"){
 // add bomb sequence
         }else{
+            this.remove();
             global.score++;
         }
     }
