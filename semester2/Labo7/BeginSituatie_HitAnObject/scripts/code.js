ea8d27da2a1cf0eadedbfd4de407ee042fb707c4
QuintBroekaert <quint.broekaert@student.vives.be>
2025-03-29 23:33:06 +0100

    update memory

--- semester2/Labo7/BeginSituatie_HitAnObject/scripts/code.js
+++ semester2/Labo7/BeginSituatie_HitAnObject/scripts/code.js
@@ -1,46 +1,72 @@
-const startGame = () => {
-    let global = {
-        IMAGE_COUNT: 5, // aantal figuren
-        IMAGE_SIZE: 48, // grootte van de figuur
-        IMAGE_PATH_PREFIX: "images/", // map van de figuren
-        IMAGE_PATH_SUFFIX: ".png", // extensie van de figuren
-        MOVE_DELAY: 3000, // aantal milliseconden voor een nieuwe afbeelding verschijnt
-        score: 0, // aantal hits
-        timeoutId: 0 // id van de timeout timer, zodat we die kunnen annuleren
-    };
-    createobjects();
+const setup = () =>{
+    let btn = document.getElementById("btn")
 }
-    const createobjects = () =>{
-        let field = document.getElementById("playField");
-        for(let index=0;index>global.IMAGE_COUNT;index++){
+
+
+var global = {
+    IMAGE_COUNT: 5,
+    IMAGE_SIZE: 48,
+    IMAGE_PATH_PREFIX: "images/",
+    IMAGE_PATH_SUFFIX: ".png",
+    MOVE_DELAY: 3000, // 3 seconds
+    score: 0,
+    interval: null,
+    FIELD_WIDTH: 600,
+    FIELD_HEIGHT: 800
+};
+
+const startGame = () => {
+    global.score = 0;
+    createObjects();
+    global.interval = setInterval(move, global.MOVE_DELAY);
+};
+
+const createObjects = () => {
+    let field = document.getElementById("playField");
+
+    for (let index = 0; index < global.IMAGE_COUNT; index++) {
         let object = document.createElement("img");
         object.src = global.IMAGE_PATH_PREFIX + index + global.IMAGE_PATH_SUFFIX;
-        object.style.width = (global.IMAGE_SIZE + "px");
-        object.style.height = (global.IMAGE_SIZE + "px");
-        object.addEventListener("click", checkState)
+        object.style.width = global.IMAGE_SIZE + "px";
+        object.style.height = global.IMAGE_SIZE + "px";
+        object.style.position = "absolute";
+
+        object.style.left = getRandomX() + "px";
+        object.style.top = getRandomY() + "px";
+
+        object.addEventListener("click", checkState);
         field.append(object);
-        global.timeoutId = setTimeout(wissel(index), global.MOVE_DELAY)
-        }
     }
-    const wissel = (index) =>{
+};
+
+const move = () => {
+    let objects = document.querySelectorAll("#playField img");
+    objects.forEach(wissel);
+};
+
+const getRandomX = () => Math.ceil(Math.random() * (global.FIELD_WIDTH - global.IMAGE_SIZE));
+const getRandomY = () => Math.ceil(Math.random() * (global.FIELD_HEIGHT - global.IMAGE_SIZE));
+
+const wissel = (object) => {
+    object.style.left = getRandomX() + "px";
+    object.style.top = getRandomY() + "px";
+};
+const updateScore = () =>{
+    
+}
+
+const checkState = (event) => {
+    let clickedImage = event.target;
+    if (clickedImage.src.includes("images/0.png")) {
+        clearInterval(global.interval);
         let field = document.getElementById("playField");
-        let objects = document.childNodes;
-        
-            let x = Math.random*800;
-            x = Math.ceil(x);
-            let y = Math.random*600;
-            y = Math.ceil(y);
-            let temp = objects[index];
-            temp.style.marginLeft = (x + "px");
-            temp.style.marginBottom = (y + "px");
-        }
-        
-    const checkState = () =>{
-        if(this.src == "images/0.png"){
-// add bomb sequence
-        }else{
-            this.remove();
-            global.score++;
-        }
+        field.innerHTML = "";
+        window.alert("Game Over!");
+    } else {
+        wissel(clickedImage);
+        global.score++;
     }
-    window.addEventListener("load",startGame)
+};
+
+window.addEventListener("load", setup);
+btn.addEventListener("click",startGame)
--- semester2/Labo7/Memory/Script/Script.js
+++ semester2/Labo7/Memory/Script/Script.js
@@ -1,38 +1,56 @@
-const startGame = () =>{
-    let global ={
-        AANTAL_HORIZONTAAL:4,
-AANTAL_VERTICAAL:3,
-AANTAL_KAARTEN:6,
-punten:0,
-beurten:0,
-previouslySelected:null
-    }
+let global = {
+    AANTAL_HORIZONTAAL: 4,
+    AANTAL_VERTICAAL: 3,
+    AANTAL_KAARTEN: 6,
+    punten: 0,
+    beurten: 0,
+    previouslySelected: null
+};
+
+const startGame = () => {
+    // zet het speelveld op.
     let table = document.getElementById("table");
     table.style.display = "grid";
-    table.style.gridTemplateColumns = "repeat(1fr," + global.AANTAL_HORIZONTAAL + ")";
-    table.style.gridTemplateRows = "repeat(1fr," + global.AANTAL_VERTICAAL + ")";
+    table.style.gridTemplateColumns = `repeat(${global.AANTAL_HORIZONTAAL}, 1fr)`;
+    table.style.gridTemplateRows = `repeat(${global.AANTAL_VERTICAAL}, 1fr)`;
+    table.style.gap = "15px";
+
     // maak de kaarten aan
-    for(let i = 0 ; i < global.AANTAL_KAARTEN; i++){
-        let card = document.createElement("img")
+    for (let i = 0; i < global.AANTAL_KAARTEN; i++) {
+        let card = document.createElement("img");
         card.alt = "this is a card";
         card.src = "images/background.png";
-        let Copy = card
-        card.addEventListener("click",select(i));
-        Copy.addEventListener("click", select(i));
+        card.dataset.cardNum = i; // Store card number
+        card.addEventListener("click", select);
+
+        let Copy = document.createElement("img");
+        Copy.alt = "this is a card";
+        Copy.src = "images/background.png";
+        Copy.dataset.cardNum = i; // Store card number
+        Copy.addEventListener("click", select);
+
         table.append(card);
         table.append(Copy);
     }
+};
 
-}
-const select = (cardNum) =>{
-    if(global.previouslySelected == null){
-        this.src = "kaart" + cardNum + ".png"
-        global.previouslySelected = this;
-     }else if(global.previouslySelected === this && previouslySelected.src !="images/background.png"){
+const select = (event) => {
+    let temp = event.currentTarget;
+    let cardNum = temp.dataset.cardNum;
+
+    if (global.previouslySelected == null) {
+        temp.src = `kaart${cardNum}.png`;
+        global.previouslySelected = temp;
+    } else if (global.previouslySelected === temp) {
         global.punten++;
-     }else{
-        this.src ="images/background.png";
-        previouslySelected.src = "images/background.png";
-        beurten++;
-     }
-}
+        global.previouslySelected = null;
+    } else {
+        setTimeout(() => {
+            temp.src = "images/background.png";
+            global.previouslySelected.src = "images/background.png";
+            global.previouslySelected = null;
+        }, 1000);
+        global.beurten++;
+    }
+};
+window.addEventListener("load",startGame)
