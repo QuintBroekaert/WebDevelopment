f1c3ae6cff9c888395366da2c18d64fc98354d2a
QuintBroekaert <quint.broekaert@student.vives.be>
2025-05-01 11:46:11 +0200

    update labo 10

---
+++ semester2/Labo10/Scripts/Script.js
@@ -0,0 +1,76 @@
+
+const initialize = () =>{
+    let btn = document.getElementById("btn");
+    if(localStorage.getItem("cards") == null){
+        localStorage.setItem("cards", JSON.stringify([]))
+    }
+    btn.addEventListener("click", parseCommmand)
+}
+const createSearchString = (array) =>{
+    let search = "";
+    for (let i = 1; i < array.length; i++){
+        search = array[i] + " "
+    }
+    search = search.trim();
+    return search;
+}
+const parseCommmand = () =>{
+    let txt = document.getElementById("txt");
+    let command = txt.value;
+    let commandWords = command.split(' ');
+    switch (commandWords[0]){
+        case "/g":
+            let g = {
+                title: "x",
+                text: txt.value,
+                url: ("https://www.google.com/search?q=" + createSearchString(commandWords))
+            };
+            window.location = g.url
+             cards = localStorage.getItem("cards")
+            cards = JSON.parse(cards);
+            cards.push(g);
+            localStorage.setItem("cards", JSON.stringify(cards));
+            break;
+                case "/i":
+                    let i = {
+                        title: "x",
+                        text: txt.value,
+                        url: ("https://www.instagram.com/explore/tags/viveshwbkortrijk/" + createSearchString(commandWords))
+                    };
+                    window.location = i.url
+                    cards = localStorage.getItem("cards")
+                    cards = JSON.parse(cards);
+                    cards.push(g);
+                    localStorage.setItem("cards", JSON.stringify(cards));
+                    break;
+                    case "/x":
+                        let x = {
+                            title: "x",
+                        text: txt.value,
+                        url: ("https://x.com/hashtag/" + createSearchString(commandWords))
+    };
+                        window.location = temp.url
+                        let cards = localStorage.getItem("cards")
+                        cards = JSON.parse(cards);
+                        cards.push(temp);
+                        localStorage.setItem("cards", JSON.stringify(cards));
+                        break;
+                        case "/y":
+                            let y = {
+                                title: "y",
+                                text: txt.value,
+                                url: ("https://www.youtube.com/results?search_query=" + createSearchString(commandWords))
+                            }
+                            window.location = y.url
+            cards = localStorage.getItem("cards")
+            cards = JSON.parse(cards);
+                            cards.push(y);
+                            localStorage.setItem("cards", JSON.stringify(cards));
+                            break;
+        default:
+            console.log(command);
+            window.alert("Invalid commmand");
+            break;
+    }
+}
+window.addEventListener("load", initialize);
