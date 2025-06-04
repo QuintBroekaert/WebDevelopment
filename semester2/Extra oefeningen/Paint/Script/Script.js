e234817c7eabda8b65fd080c6d761e3e02420a3e
QuintBroekaert <quint.broekaert@student.vives.be>
2025-06-04 17:27:24 +0200

    update paint

--- semester2/Extra oefeningen/Paint/Script/Script.js
+++ semester2/Extra oefeningen/Paint/Script/Script.js
@@ -1,6 +1,8 @@
 
+let global
 const setup = () =>{
 createpixels();
+global.selectedColor = "white"
 if(!localStorage.getItem("history")){
 localStorage.setItem("history",JSON.stringify([]))
 }
@@ -8,8 +10,19 @@ localStorage.setItem("history",JSON.stringify([]))
 const createpixels = () =>{
 for (let index = 0; index < 10000; index++) {
     let pix = document.createElement("div")
-    pix.id = index;
     pix.classList.add("pix");
+    let main = document.getElementById("field");
+    pix.id = index;
+    pix.addEventListener("click",selectpix)
+    main.append(pix);
 }
+}
+const selectpix = () =>{
+let pix = Event.Target;
+pix.style.backgroundcolor = global.selectedColor;
+}
+const selectColor = () =>{
+    let pix = Event.Target
+    global.selectColor = pix.style.backgroundcolor;
 }
 window.addEventListener("load",setup)
